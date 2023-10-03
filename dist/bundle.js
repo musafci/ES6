592fbc8394fe38ce33e63fa4a82da5220ec0ce92
Md. Abu Musa <musa.fci@gmail.com>
2023-10-03 11:21:48 +0600

    ES6 - Let vs Const vs Var Keyword

--- dist/bundle.js
+++ dist/bundle.js
@@ -7,7 +7,6 @@
  * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
  */
 /******/ (() => { // webpackBootstrap
-/******/ 	"use strict";
 /******/ 	var __webpack_modules__ = ({
 
 /***/ "./src/index.js":
@@ -16,7 +15,7 @@
   \**********************/
 /***/ (() => {
 
-eval("\n\nconsole.log(\"ES6 - Setup Working Environment with Babel and Webpack\");\n\n//# sourceURL=webpack://es6/./src/index.js?");
+eval("throw new Error(\"Module build failed (from ./node_modules/babel-loader/lib/index.js):\\nTypeError: C:\\\\laragon\\\\www\\\\ES6\\\\src\\\\index.js: Cannot read properties of null (reading 'bindings')\\n    at Scope.moveBindingTo (C:\\\\laragon\\\\www\\\\ES6\\\\node_modules\\\\@babel\\\\traverse\\\\lib\\\\scope\\\\index.js:867:13)\\n    at BlockScoping.updateScopeInfo (C:\\\\laragon\\\\www\\\\ES6\\\\node_modules\\\\babel-plugin-transform-es2015-block-scoping\\\\lib\\\\index.js:364:17)\\n    at BlockScoping.run (C:\\\\laragon\\\\www\\\\ES6\\\\node_modules\\\\babel-plugin-transform-es2015-block-scoping\\\\lib\\\\index.js:330:12)\\n    at PluginPass.BlockStatementSwitchStatementProgram (C:\\\\laragon\\\\www\\\\ES6\\\\node_modules\\\\babel-plugin-transform-es2015-block-scoping\\\\lib\\\\index.js:70:24)\\n    at newFn (C:\\\\laragon\\\\www\\\\ES6\\\\node_modules\\\\@babel\\\\traverse\\\\lib\\\\visitors.js:159:14)\\n    at NodePath._call (C:\\\\laragon\\\\www\\\\ES6\\\\node_modules\\\\@babel\\\\traverse\\\\lib\\\\path\\\\context.js:46:20)\\n    at NodePath.call (C:\\\\laragon\\\\www\\\\ES6\\\\node_modules\\\\@babel\\\\traverse\\\\lib\\\\path\\\\context.js:36:17)\\n    at NodePath.visit (C:\\\\laragon\\\\www\\\\ES6\\\\node_modules\\\\@babel\\\\traverse\\\\lib\\\\path\\\\context.js:82:31)\\n    at TraversalContext.visitQueue (C:\\\\laragon\\\\www\\\\ES6\\\\node_modules\\\\@babel\\\\traverse\\\\lib\\\\context.js:86:16)\\n    at TraversalContext.visitSingle (C:\\\\laragon\\\\www\\\\ES6\\\\node_modules\\\\@babel\\\\traverse\\\\lib\\\\context.js:65:19)\\n    at TraversalContext.visit (C:\\\\laragon\\\\www\\\\ES6\\\\node_modules\\\\@babel\\\\traverse\\\\lib\\\\context.js:109:19)\\n    at traverseNode (C:\\\\laragon\\\\www\\\\ES6\\\\node_modules\\\\@babel\\\\traverse\\\\lib\\\\traverse-node.js:22:17)\\n    at traverse (C:\\\\laragon\\\\www\\\\ES6\\\\node_modules\\\\@babel\\\\traverse\\\\lib\\\\index.js:52:34)\\n    at transformFile (C:\\\\laragon\\\\www\\\\ES6\\\\node_modules\\\\@babel\\\\core\\\\lib\\\\transformation\\\\index.js:82:31)\\n    at transformFile.next (<anonymous>)\\n    at run (C:\\\\laragon\\\\www\\\\ES6\\\\node_modules\\\\@babel\\\\core\\\\lib\\\\transformation\\\\index.js:24:12)\\n    at run.next (<anonymous>)\\n    at transform (C:\\\\laragon\\\\www\\\\ES6\\\\node_modules\\\\@babel\\\\core\\\\lib\\\\transform.js:22:33)\\n    at transform.next (<anonymous>)\\n    at step (C:\\\\laragon\\\\www\\\\ES6\\\\node_modules\\\\gensync\\\\index.js:261:32)\\n    at C:\\\\laragon\\\\www\\\\ES6\\\\node_modules\\\\gensync\\\\index.js:273:13\\n    at async.call.result.err.err (C:\\\\laragon\\\\www\\\\ES6\\\\node_modules\\\\gensync\\\\index.js:223:11)\\n    at C:\\\\laragon\\\\www\\\\ES6\\\\node_modules\\\\gensync\\\\index.js:189:28\\n    at C:\\\\laragon\\\\www\\\\ES6\\\\node_modules\\\\@babel\\\\core\\\\lib\\\\gensync-utils\\\\async.js:68:7\\n    at C:\\\\laragon\\\\www\\\\ES6\\\\node_modules\\\\gensync\\\\index.js:113:33\\n    at step (C:\\\\laragon\\\\www\\\\ES6\\\\node_modules\\\\gensync\\\\index.js:287:14)\\n    at C:\\\\laragon\\\\www\\\\ES6\\\\node_modules\\\\gensync\\\\index.js:273:13\\n    at async.call.result.err.err (C:\\\\laragon\\\\www\\\\ES6\\\\node_modules\\\\gensync\\\\index.js:223:11)\");\n\n//# sourceURL=webpack://es6/./src/index.js?");
 
 /***/ })
 
@@ -25,7 +24,7 @@ eval("\n\nconsole.log(\"ES6 - Setup Working Environment with Babel and Webpack\"
 /******/ 	
 /******/ 	// startup
 /******/ 	// Load entry module and return exports
-/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	// This entry module doesn't tell about it's top-level declarations so it can't be inlined
 /******/ 	var __webpack_exports__ = {};
 /******/ 	__webpack_modules__["./src/index.js"]();
 /******/ 	
--- src/index.js
+++ src/index.js
@@ -1,1 +1,66 @@
-console.log("ES6 - Setup Working Environment with Babel and Webpack");
+/** 
+ * Const is a keyword in JavaScript which is used to declare a constant value.
+*/
+
+/*
+    const x = 10;
+    x = 20; // This will throw an error, because we are trying to reassign a value to a constant variable.
+    console.log(x);
+*/
+
+
+/*
+    const arr =[];
+    arr.push(10);
+    arr.push(20);
+
+    arr = []; // This will throw an error, because we are trying to reassign a value to a constant variable.
+
+    console.log(arr); // This will print [10, 20] to the console.
+*/
+
+
+
+/** 
+ * Let vs Const vs Var 
+*/
+
+
+/*
+    var num = 10;
+    if(true) {
+        var num = 20; // This will override the value of num to 20.
+        console.log(num); // This will print 20 to the console.
+    }
+    console.log(num); // This will print 20 to the console.
+*/
+
+
+
+/*
+    let num = 10;
+    if (true) {
+        let num = 20; // This will not override the value of num to 20.
+        console.log(num) // This will print 20 to the console.
+    }
+    console.log(num); // This will print 10 to the console.
+*/
+
+
+/*
+    function demo() {
+        {
+            let num = 10;
+            console.log(num); // This will print 10 to the console.
+            {
+                const num = 20;
+                console.log(num); // This will print 20 to the console.
+            }
+            num = 30;
+            console.log(num); // This will print 30 to the console.
+        }
+        console.log(num); // This will throw an error, because num is not defined.
+    }
+
+    demo();
+*/
